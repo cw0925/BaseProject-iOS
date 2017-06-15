73e04f55b69154d22fec9deabd1d4809480fd6ba
cw0925 <294760125@qq.com>
2017-06-15 10:28:14 +0800

    Initial Commit

---
+++ RaiseLearn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RaiseLearn
+//
+//  Created by afarsoft on 2017/6/15.
+//  Copyright © 2017年 afarsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
